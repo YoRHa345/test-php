9160a8c337abeb97d1809a58d899be73a9d9e842
YoRHa345 <166796945+YoRHa345@users.noreply.github.com>
2025-01-05 17:00:22 +0900

    農園名変更

--- staff/staff_add_done.php
+++ staff/staff_add_done.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>kazuki農園</title>
+    <title>ほげほげ農園</title>
 </head>
 <body>
     <?php
